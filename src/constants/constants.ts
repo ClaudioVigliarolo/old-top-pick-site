b5981ec1ee555a083b9fee7b6af4e4d412c21fc9
Claudio Vigliarolo <claudio.vigliarolo@kerr-italy.it>
2021-09-07 12:31:51 +0200

    completed paypal & get-app

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,5 +3,5 @@ export const DEFAULT_LEVEL = -1;
 export const API_HOSTNAME = "https://top-pick-api.herokuapp.com"; //" http://localhost:4001";;
 export const SITE_HOSTNAME =
   process.env.NODE_ENV === "production"
-    ? "https://top-pick.com"
+    ? "https://top-pick.app"
     : "http://localhost:8080";
